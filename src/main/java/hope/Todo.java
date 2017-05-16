4dd1fb899bce5620bb6d0fc7dbece329447b53c1
Renan Martins <martins@fujisan.co.jp>
2017-05-16 12:39:04 -0700

    Add not null constraint for Todo class

--- src/main/java/hope/Todo.java
+++ src/main/java/hope/Todo.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Todo {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+    @NotNull
     private String content;
 
     public Integer getId() {
--- src/main/java/hope/TodoController.java
+++ src/main/java/hope/TodoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -64,14 +65,14 @@ public class TodoController {
     }
 
     @PostMapping("")
-    public ResponseEntity<?> save(@RequestBody Todo todo) {
+    public ResponseEntity<?> save(@Validated @RequestBody Todo todo) {
         Todo savedTodo = todoRepository.save(todo);
 
         return new ResponseEntity(savedTodo, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public @ResponseBody ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Todo todoBody) {
+    public @ResponseBody ResponseEntity<?> update(@PathVariable Integer id, @Validated @RequestBody Todo todoBody) {
         Todo todo = todoRepository.findOne(id);
 
         if (todo == null) {
