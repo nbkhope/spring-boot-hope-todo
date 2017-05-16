9d997de22b58e699e5808120a71d33a2469f0e83
Renan Martins <martins@fujisan.co.jp>
2017-05-16 10:10:25 -0700

    Add HttpError class and use it to return 404 from show action

---
+++ src/main/java/hope/HttpError.java
@@ -0,0 +1,27 @@
+package hope;
+
+public class HttpError {
+    private Integer code;
+    private String message;
+
+    HttpError(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/hope/TodoController.java
+++ src/main/java/hope/TodoController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import hope.Todo; 
 import hope.TodoRepository; 
@@ -26,8 +29,16 @@ public class TodoController {
     }
 
     @GetMapping(path="/{id}")
-    public @ResponseBody Todo show(@PathVariable Integer id) {
-        return todoRepository.findOne(id);
+    public @ResponseBody ResponseEntity<?> show(@PathVariable Integer id) {
+        Todo todo = todoRepository.findOne(id);
+
+        if (todo == null) {
+            // return ResponseEntity.notFound().build();
+            HttpError httpError = new HttpError(404, "Resource " + id + " not found");
+            return new ResponseEntity(httpError, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(todoRepository.findOne(id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
