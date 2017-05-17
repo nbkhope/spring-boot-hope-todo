d2c3a65041002afa13680bd40b417b109e1d2f67
Renan Martins <martins@fujisan.co.jp>
2017-05-17 12:55:15 -0700

    Add initial dummy data

--- src/main/java/hope/Application.java
+++ src/main/java/hope/Application.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.boot.CommandLineRunner;
+import java.util.Arrays;
+
+import hope.model.Account;
+import hope.model.Todo;
+import hope.repository.AccountRepository;
+import hope.repository.TodoRepository;
 
 @SpringBootApplication
 public class Application {
@@ -17,5 +24,21 @@ public class Application {
         messageBundle.setBasename("classpath:messages/messages");
         messageBundle.setDefaultEncoding("UTF-8");
         return messageBundle;
+    }
+
+    // BootStrap :) Initialize dummy data here
+    @Bean
+    CommandLineRunner init(
+        AccountRepository accountRepository,
+        TodoRepository todoRepository
+    ) {
+        return (evt) -> Arrays.asList("asmith,kyamada,rrivera".split(","))
+            .forEach(username-> {
+                Account newAccount = new Account(username, "password123");
+                Account account = accountRepository.save(newAccount);
+
+                todoRepository.save(new Todo(account, "Do the dishes"));
+                todoRepository.save(new Todo(account, "Walk the dog"));
+            });
     }
 }
