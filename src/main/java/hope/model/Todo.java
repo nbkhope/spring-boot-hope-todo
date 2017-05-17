4b753044bbe4cede722904e2f4ddd7a14e45baf5
Renan Martins <martins@fujisan.co.jp>
2017-05-17 12:06:04 -0700

    Explicitly name table todos using @Table annotation

--- src/main/java/hope/model/Todo.java
+++ src/main/java/hope/model/Todo.java
@@ -2,11 +2,13 @@ package hope.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name="todos")
 public class Todo {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
