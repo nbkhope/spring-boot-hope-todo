cd33848aa61aa736f1da560bbd803815a2ac4cfd
Renan Martins <martins@fujisan.co.jp>
2017-05-17 17:06:24 -0700

    Add setters for password and username

--- src/main/java/hope/model/Account.java
+++ src/main/java/hope/model/Account.java
@@ -50,6 +50,14 @@ public class Account {
         return username;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     Account() { // jpa only
     }
 
